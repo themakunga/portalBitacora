d3891e1ba66bed09971cc600834ac8977b33ed57
kojimairiguchi <nicolas.martinez@humandaft.cl>
2015-10-28 21:38:35 -0300

    0.32.11

--- sections/activas.php
+++ sections/activas.php
@@ -4,13 +4,13 @@ $ultimas = entradasEjecucion();
 ?>
 <div class="col-lg-12">
 <h3>Entradas Activas</h3>
-<table class="table table-striped table-hover">
+<table class="table table-fixed">
 	<thead>
 		<tr>
 			<th class="col-xs-1">ID</th>
-			<th class="col-xs-8">Descripcion</th>
+			<th class="col-xs-7">Descripcion</th>
 			<th class="col-xs-1">Desde</th>
-			<th class="col-xs-1">Estatus</th>
+			<th class="col-xs-2">Estatus</th>
 			<th class="col-xs-1"></th>
 		</tr>
 	</thead>
@@ -19,9 +19,9 @@ $ultimas = entradasEjecucion();
 			while($lista = mysql_fetch_array($ultimas)){
 				echo '<tr>
 						<td class="col-xs-1">'.$lista['id'].'</td>
-						<td class="col-xs-8">'.$lista['descripcion'].'</td>
+						<td class="col-xs-7">'.$lista['descripcion'].'</td>
 						<td class="col-xs-1">'.$lista['inicio'].'</td>
-						<td class="col-xs-1">'.iconoPend($lista['id']).'</td>
+						<td class="col-xs-2">'.iconoPend($lista['id']).'</td>
 						<td class="col-xs-1"><a href=javascript:newPopup("sections/popup-edit.php?edit='.$lista['id'].'"); class="btn btn-xs btn-success">Validar</a></td>
 					</tr>';
 			}
